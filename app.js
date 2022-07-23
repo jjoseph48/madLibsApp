25baeccda5b621df2bff3936970018b1e2f19f36
jjoseph48 <99454751+jjoseph48@users.noreply.github.com>
2022-07-23 17:04:10 +0700

    Ubah beberapa perintah ke bahasa Indonesia
    Perubahan pada nilai numberOfQuestion, penambahan 'greetings'

--- app.js
+++ app.js
@@ -1,44 +1,58 @@
 "use strict";
+// Jumlah pertanyaan
+let numberOfQuestion = 7;
 
-let numberOfQuestion = 8;
-
+// Penunjuk pertanyaan
 let questionCounter = 0;
 
-let questionArray = ["Masukkan kata benda :", "Masukkan kata kerja :", "Masukkan kata benda :", "Masukkan nama hewan :", "Masukkan kata kerja :", "Masukkan nama hewan :", 
-"Masukkan kata kerja :","Masukkan kata kerja :"];
+// List pertanyaan
+let greetings = 'Tolong masukkan ';
+let questionArray = [
+    ` ${greetings} kata kerja `, 
+    ` ${greetings} kata kerja `, 
+    ` ${greetings} nama bunga `, 
+    ` ${greetings} kata kerja `, 
+    ` ${greetings} nama hewan `, 
+    ` ${greetings} teman / orang tua / pacar `, 
+    ` ${greetings} kata kerja `,
+    ` ${greetings} nama tumbuhan `
+];
     
 let userInputs = [];
-for (let i = numberOfQuestion-1; i >= 0; i--){
+for (let i = numberOfQuestion; i >= 0; i--) {
     console.log(
-        questionArray[questionCounter] + ' ... You have (${questiotnTotal}) questions left'
+      questionArray[questionCounter] + `... tersisa (${numberOfQuestion} pertanyaan)`
     );
     userInputs.push(
-        prompt(
-            questionArray[questionCounter] + ' ... You have (${questiotnTotal}) questions left'
-        )
+      prompt(
+        questionArray[questionCounter] + `... tersisa (${numberOfQuestion} pertanyaan)`
+      )
     );
     console.log(userInputs);
     questionCounter++;
     numberOfQuestion--;
-}
+  }
         
-let originalStory = `Seorang laki-laki melihat {userInputs[0]} besar yang terjepit batu. Ular itu tidak bisa melepaskan badannya. 
-    Laki-laki itu lalu {userInputs[1]} batunya agar ular bisa melepaskan diri. Setelah batu diangkat, ular Iangsung melilit tubuh 
-    laki-laki itu hendak memangsanya. “Tunggu! Mengapa kau lakukan itu? Bukankah aku telah menolongmu?” kata si Laki-laki.
+let originalStory = `<h3>Seorang laki-laki melihat ${userInputs[0]} besar yang terjepit batu. Ular itu tidak bisa melepaskan badannya. 
+    Laki-laki itu lalu ${userInputs[1]} batunya agar ular bisa melepaskan diri. Setelah batu diangkat, ular Iangsung melilit tubuh 
+    laki-laki itu hendak memangsanya. </h3>
+    <h3>“Tunggu! Mengapa kau lakukan itu? Bukankah aku telah menolongmu?” kata si Laki-laki.
     “Aku lapar dan tidak ada salahnya aku memangsamu,” kata ular yang tak tahu membalas budi.“Kau jangan makan aku sekarang. 
-    Kita tanyakan dulu {userInputs[2]} kita pada binatang yang bijaksana,” kata si laki-laki kepada ular. Lalu, mereka berjalan dan 
-    bertemu {userInputs[3]}.“Apakah pantas ular ingin memangsaku? Padahal, aku sudah melepaskannya dari impitan batu,” tanya si laki-laki 
+    Kita tanyakan dulu ${userInputs[2]} kita pada binatang yang bijaksana,” kata si laki-laki kepada ular.</h3>
+    <h3>Lalu, mereka berjalan dan bertemu ${userInputs[3]}.“Apakah pantas ular ingin memangsaku? Padahal, aku sudah melepaskannya dari impitan batu,” tanya si laki-laki 
     kepada hyena. Hyena berpikir, jika memihak ular, ia bisa ikut menikmati daging laki-laki itu. Hyena pun menjawab, “Tidak ada salahnya.”
-    Mendengar jawaban hyena, ular hendak {userInputs[4]} si laki-laki. Tiba-tiba, laki-laki itu berkata, “Tunggu dulu, kita tanya pada 
-    binatang yang lain.” Mereka lalu bertemu {userInputs[5]}. “Apakah pantas Ular ingin memangsaku? Padahal, aku sudah meIepaskannya dari impitan 
+    Mendengar jawaban hyena, ular hendak ${userInputs[4]} si laki-laki. Tiba-tiba, laki-laki itu berkata, “Tunggu dulu, kita tanya pada 
+    binatang yang lain.”</h3>
+    <h3>Mereka lalu bertemu ${userInputs[5]}. “Apakah pantas Ular ingin memangsaku? Padahal, aku sudah meIepaskannya dari impitan 
     batu,” tanya si laki-laki kepada serigala.Serigala menjawab, “Aku tidak percaya seekor ular bisa terimpit batu. Coba kau tunjukkan 
-    aku bagaimana kejadiannya.” Mereka bertiga pergi ke tempat semula. Serigala berkata, “Coba kau kembali ke tempatmu.” Ular kembali ke 
-    tempatnya. Lalu, si laki-laki {userInputs[6]} ular dengan batu. Ternyata benar, ular terlihat tidak bisa melepaskan dirinya. Si laki-laki 
-    hendak melepaskan ular kembali. “Jangan {userInputs[7]}!” kata serigala. “Ular itu akan memangsamu. Biarkan dia di situ,” ucap serigala kembali.`
+    aku bagaimana kejadiannya.” Mereka bertiga pergi ke tempat semula.</h3>
+    <h3>Serigala berkata, “Coba kau kembali ke tempatmu.” Ular kembali ke 
+    tempatnya. Lalu, si laki-laki ${userInputs[6]} ular dengan batu. Ternyata benar, ular terlihat tidak bisa melepaskan dirinya. Si laki-laki 
+    hendak melepaskan ular kembali. “Jangan ${userInputs[7]}!” kata serigala. “Ular itu akan memangsamu. Biarkan dia di situ,” ucap serigala kembali.</h3>`
 console.log(originalStory);
 
 alert(
-    "All done! Ready for your totally-accurate, not-at-all confusing history of tech??"
+    "Pertanyaan habis! Ini adalah cerita yang telah Anda `akurasikan`"
   );
 
   // Output
